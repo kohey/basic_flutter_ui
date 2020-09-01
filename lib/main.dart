40f2f4c447349f5752b9401b15d006590b702e8a
kohey <kawasonblog@gmail.com>
2020-09-01 16:54:35 +0900

    具体的なボタンを生成

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,17 @@ class MyApp extends StatelessWidget {
         ],
       ),
     );
+    Color color = Theme.of(context).primaryColor;
 
+    Widget buttonSection = Container(
+        child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    ));
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -69,6 +79,7 @@ class MyApp extends StatelessWidget {
           body: Column(
             children: [
               titleSection,
+              buttonSection,
             ],
           )),
     );
