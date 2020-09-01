0d4a5970ce708ef6b1e150dea683a65aaac3723f
kohey <kawasonblog@gmail.com>
2020-09-01 14:28:33 +0900

    ボタンとテキストのペア生成のヘルパーを追加

--- lib/main.dart
+++ lib/main.dart
@@ -73,6 +73,28 @@ class MyApp extends StatelessWidget {
           )),
     );
   }
+
+  // ボタン作成の helper
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        )
+      ],
+    );
+  }
 }
 
 class MyHomePage extends StatefulWidget {
